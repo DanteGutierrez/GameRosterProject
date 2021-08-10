16a81985f0a19acc3c7fe6ef782aa50760493c71
DanteGutierrez <78101352+DanteGutierrez@users.noreply.github.com>
2021-08-10 10:06:35 -0600

    Day One and a half

--- Dante/minesweeperScript.js
+++ Dante/minesweeperScript.js
@@ -1,17 +1,18 @@
 let row = 3;
 let column = 4;
 let grid = [];
+let mines = 50;
 
 const loadBoard = () => {
     var board = document.getElementById("Board");
     board.innerHTML = '';
     grid = [];
     for (var r = 0; r < row; r++) {
-        board.innerHTML += '<div class="row"></div>';
+        board.innerHTML += '<div class="row boardRow"></div>';
         grid[r] = [];
         for (var c = 0; c < column; c++) {
             grid[r][c] = 0;
-            document.getElementsByClassName("row")[r].innerHTML += '<div class="box"></div>';
+            document.getElementsByClassName("boardRow")[r].innerHTML += '<div class="box"></div>';
         }
     }
 }
@@ -19,4 +20,20 @@ loadBoard();
 document.getElementById("AddColumn").addEventListener("click", (evt) => {
     column++;
     loadBoard();
+});
+document.getElementById("AddRow").addEventListener("click", (evt) => {
+    row++;
+    loadBoard();
+});
+document.getElementById("SubColumn").addEventListener("click", (evt) => {
+    if (column > 1) {
+        column--;
+        loadBoard();
+    }
+});
+document.getElementById("SubRow").addEventListener("click", (evt) => {
+    if (row > 1) {
+        row--;
+        loadBoard();
+    }
 });
