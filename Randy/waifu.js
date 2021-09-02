158e27ab77fcdceb0e8c5458d7c20abb601f10b5
RandyStill-36 <randystill2001@gmail.com>
2021-09-01 22:22:53 -0600

    restructured

--- Randy/waifu.js
+++ Randy/waifu.js
@@ -2,20 +2,17 @@ var money = 0;
 var multiplier = 1;
 var BaseClick = 1;
 var start = 0;
-var html;
 
 let Timer;
-let Click;
+let Click = 0;
 let Automult = 1;
 let Clickinterval = 1000;
 
-let HTML;
+let playerName;
 let money_btn;
 let money_count;
 let UpgradePage;
-let Background1;
-// let Waifu1;
-let Waifu2;
+let Waifu;
 let MainPage;
 let Upgrade1;
 let Upgrade2;
@@ -41,34 +38,11 @@ let UpgradesPageOnClick = evt =>
     sessionStorage.setItem('money',money);
     sessionStorage.setItem('multiplier',multiplier);
     sessionStorage.setItem('BaseClick',BaseClick);
+    sessionStorage.setItem('Timer',Timer);
+    sessionStorage.setItem('Click',Click);
+    sessionStorage.setItem('Clickinterval',Clickinterval);
+    sessionStorage.setItem('Automult',Automult);
     console.log(sessionStorage.getItem('multiplier'));
-  }
-}
-
-let Background1onClick = evt =>
-{
-  if(evt.target.id == 'Background1')
-  {
-    document.body.style.backgroundImage = "url('images/anime-City.jpg')";
-    document.body.style.backgroundSize = "cover";
-  }
-}
-
-let Waifu1onClick = evt =>
-{
-  if(evt.target.id == 'Waifu1')
-  {
-    document.getElementById('waifu').classList.remove();
-    document.getElementById('waifu').classList.add('waifu1');
-  }
-}
-
-let Waifu2onClick = evt =>
-{
-  if(evt.target.id == 'Waifu2')
-  {
-    document.getElementById('waifu').classList.remove();
-    document.getElementById('waifu').classList.add('waifu2');
   }
 }
 
@@ -80,6 +54,10 @@ let MainPageOnClick = evt =>
     sessionStorage.setItem('money',money);
     sessionStorage.setItem('multiplier',multiplier);
     sessionStorage.setItem('BaseClick',BaseClick);
+    sessionStorage.setItem('Timer',Timer);
+    sessionStorage.setItem('Click',Click);
+    sessionStorage.setItem('Clickinterval',Clickinterval);
+    sessionStorage.setItem('Automult',Automult);
     console.log(sessionStorage.getItem('money'));
     sessionStorage.setItem('start', start);
     console.log(sessionStorage.getItem('start'));
@@ -126,6 +104,11 @@ let Upgrade3OnClick = evt =>
   }
 }
 
+let timmermethod = () => {
+  money = money + (Click * Automult);
+  money_count.innerHTML = money;
+};
+
 let AutoUpgrade1OnClick = evt =>
 {
   if(evt.target.id == 'autoUpgrade1')
@@ -136,8 +119,8 @@ let AutoUpgrade1OnClick = evt =>
       Automult = 1;
       Click = Click + 1;
       
-      Timer = setInterval(function(){money = money + (Click * Automult)},Clickinterval);
-      console.log("I stole money");
+      Timer = setInterval(timmermethod, Clickinterval);
+      console.log(Click);
     }
   }
 }
@@ -151,6 +134,7 @@ let PowerUpgrade1OnClick = evt =>
       money_count.innerHTML = money;
       multiplier = 1;
       BaseClick = BaseClick + 2;
+      clearInterval(Timer);
       console.log(multiplier);
       console.log(BaseClick);
     }
@@ -164,27 +148,25 @@ function runtime(){
     money_btn = document.getElementById('waifu');
     money_count = document.getElementById('money');
     UpgradePage = document.getElementById('UpgradesPage');
-    Background1 = document.getElementById('Background1');
-    // Waifu1 = document.getElementById('Waifu1');
-    Waifu2 = document.getElementById('Waifu2');
     if(sessionStorage.getItem('start') == 1){
       money = Number(sessionStorage.getItem('money'));
       multiplier = Number(sessionStorage.getItem('multiplier'));
       BaseClick = Number(sessionStorage.getItem('BaseClick'));
+      Timer = Number(sessionStorage.getItem('Timer'));
+      Click = Number(sessionStorage.getItem('Click'));
+      Automult = Number(sessionStorage.getItem('Automult'));
+      Clickinterval = Number(sessionStorage.getItem('Clickinterval'));
+
       money_count.innerHTML = money;
       console.log(money);
 
+      //();
     }else if(start == 0){
       money_count.innerHTML = money;
     }
    
-
     money_btn.addEventListener('click', onClick);
     UpgradePage.addEventListener('click', UpgradesPageOnClick);
-    Background1.addEventListener('click',Background1onClick);
-    // Waifu1.addEventListener('click',Waifu1onClick);
-    Waifu2.addEventListener('click',Waifu2onClick);
-
     
   }else if(document.URL.includes("Upgrades.html")){
     start = 1;
@@ -198,6 +180,11 @@ function runtime(){
     money = Number(sessionStorage.getItem('money'));
     multiplier = Number(sessionStorage.getItem('multiplier'));
     BaseClick = Number(sessionStorage.getItem('BaseClick'));
+    Timer = Number(sessionStorage.getItem('Timer'));
+    Click = Number(sessionStorage.getItem('Click'));
+    Automult = Number(sessionStorage.getItem('Automult'));
+    Clickinterval = Number(sessionStorage.getItem('Clickinterval'));
+    //();
     
     MainPage.addEventListener('click', MainPageOnClick);
     Upgrade1.addEventListener('click', Upgrade1OnClick);
